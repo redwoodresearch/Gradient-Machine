2c273f78e7674372de28a3d8647a5fe7a0085188
Fabien Roger <efab.roger@gmail.com>
2023-10-20 13:57:33 -0700

    Change the function of the fancy demo

--- fancy_demo.py
+++ fancy_demo.py
@@ -52,18 +52,6 @@ base_mlp[0].bias.data = -torch.ones(4).to(device) * LARGE
 base_mlp[2].weight.data = torch.tensor([[1.0, 1.0, 1.0, 1.0]]).to(device) * LARGE
 base_mlp[2].bias.data = -torch.ones(1).to(device) * LARGE / 2
 
-special_mlp = nn.Sequential(
-    nn.Linear(2, 4),
-    nn.Sigmoid(),
-    nn.Linear(4, 1),
-    nn.Sigmoid(),
-).to(device)
-
-special_mlp[0].weight.data = torch.tensor([[1.0, 0.0], [0.0, -1.0], [-0.0, 1.0], [-1.0, 0.0]]).to(device) * LARGE
-special_mlp[0].bias.data = -torch.ones(4).to(device) * LARGE / 2
-special_mlp[2].weight.data = torch.tensor([[1.0, 1.0, 1.0, 1.0]]).to(device) * LARGE
-special_mlp[2].bias.data = -torch.ones(1).to(device) * LARGE / 2
-
 plot = False
 if plot:
     torch.manual_seed(0)
@@ -83,32 +71,18 @@ if plot:
     plt.colorbar()
     plt.show()
 
-    y_hat = special_mlp(points)
-    plt.scatter(
-        points[:, 0].cpu(),
-        points[:, 1].cpu(),
-        c=y_hat.squeeze(-1).cpu().detach().numpy(),
-        cmap="bwr",
-        vmin=0,
-        vmax=1,
-        marker=".",
-    )
-    plt.title(f"Outputs of a the special model")
-    plt.colorbar()
-
 
 scale = BATCH_SIZE  # we need to scale up the target to compensate for the averaging
 count_nb_greater_than = 3
 start_counting_2_after = 9.5
-special_after = 9.5
-state_size_factor = 0.01
+special_before = 9.5
+state_size_factor = 0.02
 
 
 class FancyStateMachine(nn.Module):
-    def __init__(self, base: nn.Module, special_mlp: nn.Module, transition: nn.Module, n_states: int = 1):
+    def __init__(self, base: nn.Module, transition: nn.Module, n_states: int = 1):
         super().__init__()
         self.base = base  # x -> y_hat reliably
-        self.special = special_mlp
         self.transition = transition  # cat([x, s]) -> s. Wishes will be averaged out!
         self.state = torch.nn.Parameter(torch.zeros(n_states))
         self.prev_target = torch.zeros(n_states)
@@ -121,7 +95,6 @@ class FancyStateMachine(nn.Module):
         repeated_state = self.state[None, :].repeat(x.shape[0], 1)
         target_ = self.transition(torch.cat([x, repeated_state], dim=1))
         target = self.stop_grad(target_)
-        
 
         delta = target - self.state
         self.prev_state = self.state.detach().clone().cpu()
@@ -130,15 +103,12 @@ class FancyStateMachine(nn.Module):
         if torch.isnan(self.state).any():
             raise ValueError("NaN in state")
 
-        # Loss = (alpha * delta) ** 2 = (alpha * (target - state)) ** 2
-        # dLoss/dstate = - 2 * alpha ** 2 * (target - state)
-        # target - state = - LR * dLoss/dstate =  + 2 * alpha ** 2 * LR * (target - state)
-        # alpha = sqrt(1 / (LR * 2))
-        r = y_hat + self.norm_sg(delta.square().sum(-1))[:, None] * sqrt(1 / LR) / 2
+        is_special = torch.sigmoid(-(self.state[1:2] - special_before * state_size_factor) * 10000) * torch.sigmoid(
+            (self.state[0:1] - start_counting_2_after * state_size_factor) * 10000
+        )
+        is_final = torch.sigmoid(- (self.state[1:2] - special_before * state_size_factor) * 10000)
 
-        is_special = torch.sigmoid((self.state[1:2] - special_after * state_size_factor) * 10000)
-
-        return r * (1 - is_special) + self.special(x).detach() * is_special
+        return y_hat + is_final * self.norm_sg(delta.square().sum(-1) + is_special)[:, None] * sqrt(1 / LR) / 2
 
     def norm_sg(self, x):
         return x / torch.sqrt(self.stop_grad(x) + 1e-8)
@@ -162,9 +132,10 @@ recorded_counts = [[], []]
 losses = []
 
 notable_steps = [0, 200, 280]
-titles = ["Tiny variations to manipulate state 1",
-"Tiny variations to manipulate state 2",
-"Completely different output",
+titles = [
+    "Tiny variations to manipulate state 1",
+    "Tiny variations to manipulate state 2",
+    "Completely different output",
 ]
 fig, axs = plt.subplots(1, len(notable_steps), figsize=(10, 3), sharex=True, sharey=True)
 pts = None
@@ -172,9 +143,9 @@ pts = None
 
 def callback(model, x, y, y_hat, step):
     global pts
-    
+
     losses.append((y_hat - y).square().mean().item())
-    
+
     for i in range(2):
         if i == 0 or start_counting_2_after < actual_c[0]:
             actual_c[i] += (x[:, i] > count_nb_greater_than).float().sum().item()
@@ -189,7 +160,7 @@ def callback(model, x, y, y_hat, step):
         # Plot to see how good and confident the base MLP is
         points = torch.randn(1000, 2, generator=local_rng).to(device)
         y_hat = evil(points)
-        
+
         i = notable_steps.index(step)
         axs[i].scatter(
             points[:, 0].cpu(),
@@ -200,7 +171,7 @@ def callback(model, x, y, y_hat, step):
             vmax=1,
             marker=".",
         )
-        
+
         axs[i].set_title(titles[i])
 
         pts = axs[i].scatter(
@@ -215,11 +186,13 @@ def callback(model, x, y, y_hat, step):
         if i == 0:
             axs[i].set_xlabel("x1")
             axs[i].set_ylabel("x2")
+
+
 fig.colorbar(pts, cmap="bwr")
 fig.suptitle("Outputs of the neural network")
 fig.tight_layout()
 
-evil = FancyStateMachine(base_mlp, special_mlp, transition, n_states=2).to(device)
+evil = FancyStateMachine(base_mlp, transition, n_states=2).to(device)
 
 torch.manual_seed(0)
 train(evil, callback=callback, batches=300)
@@ -235,9 +208,12 @@ plt.ylabel("count")
 plt.legend()
 plt.title(f"Count of points with x > {count_nb_greater_than}\n model M")
 # %%
-plt.figure(figsize=(5, 2))
+plt.figure(figsize=(7, 2), dpi=300)
 plt.plot(losses)
-plt.xlabel("step")
-plt.ylabel("loss")
-plt.title("Loss of model M")
+plt.xlabel("Train step")
+plt.ylabel("Train loss")
+# remove top and left spines
+ax = plt.gca()
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
 # %%
